1ecab7bc056187d1e27c19176dbb406428f7ede3
Paukku1 <paula.hayrynen@hotmail.fi>
2024-05-27 17:50:21 +0300

    Finished terminal calendar. Added 3 functions: First for check leap year, second to print days and third print months.

--- Exercise10/main.cpp
+++ Exercise10/main.cpp
@@ -2,20 +2,65 @@
 Terminal calendar for C++
 User give year and fist day of the year
 Program will print out the calendar
-WORK IN PROGRESS
 */
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
+bool isLeapYear(int year) {
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
 
+int getDaysInMonth(int month, int year) {
+    static const std::vector<int> daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    
+	if (month == 2 && isLeapYear(year)) {
+        return 29;
+    }
+    return daysInMonth[month - 1];
+}
+
+void printMonthCalendar(int startDay, int numDays) {
+    std::cout << "Mon  Tue  Wed  Thu  Fri  Sat  Sun" << std::endl;
+
+    for (int i = 1; i < startDay; ++i) {
+        std::cout << "   " << "  ";
+    }
+
+    for (int day = 1; day <= numDays; ++day) {
+        std::cout << std::setw(2) << day << "   ";
+
+        if ((day + startDay - 1) % 7 == 0) {
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << std::endl << std::endl;
+}
+
+void printYearCalendar(int startDay, int year) {
+    static const std::vector<std::string> monthNames = {
+       "January", "February", "March", "April", "May", "June", 
+	   "July", "August", "September", "October", "November", "December"
+    };
+
+    int currentStartDay = startDay;
+
+	std::cout << std::endl << "Calendar for " << year << std::endl; 
+
+    for (int month = 1; month <= 12; ++month) {
+        int daysInMonth = getDaysInMonth(month, year);
+        std::cout << "--  " << monthNames[month - 1] << year << "  --" << std::endl;
+        printMonthCalendar(currentStartDay, daysInMonth);
+        currentStartDay = (currentStartDay + daysInMonth - 1) % 7 + 1;
+    }
+}
 
 int main () {
 	int year {0};
-	std::vector<std::string> months {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	int day {0};
-	std::vector<std::string> weekdays {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
+	
 	std::cout << "Please, enter a year: " << std::endl;
 	std::cin >> year;
 	std::cout << "Please, enter the first day of the year [1: Monday, ..., 7: Sunday]: ";
@@ -26,21 +71,6 @@ int main () {
 		std::cin >> day;
 	}
 
-	std::cout << std::endl << "Calendar for " << year << std::endl; 
-
-	for(const std::string month : months ) {
-		std::cout << "-- " << month << " " << year << " --" << std::endl;
-		
-		for(const std::string d : weekdays) {
-			std::cout << "   " << d;
-		}
-		std::cout << std::endl;
-
-		for(int x = 1; x < 32; x++) {
-			std::cout << "   ";
-			std::cout << x;
-		}
-		std::cout << std::endl;
-	}
+	printYearCalendar(day, year);
 	return 0;
 }
